d4ce16aeb5fd5c278ff026ddad4972f6deb006e5
Strongreen <strongreen@live.com>
2020-09-24 14:49:02 -0300

    Projeto em dupla [Strongreen-Damiem]

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let btnEntrar = document.querySelector("#button-login");
 let campEmailTel = document.querySelector("#user-email-phone");
 
-btnEntrar.addEventListener('click', function() {
-    alert(campEmailTel.value);
-})
+btnEntrar.addEventListener("click", function () {
+  alert(campEmailTel.value);
+});
