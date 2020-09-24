cbde2b106b490abcca654283648ff0cc9a4e80fc
Strongreen <strongreen@live.com>
2020-09-24 12:57:40 -0300

    Como diz Oliva, Fazer o em Nome do Pai aqui

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnEntrar = document.querySelector("#button-login");
+let campEmailTel = document.querySelector("#user-email-phone");
+
+btnEntrar.addEventListener('click', function() {
+    alert(campEmailTel.value);
+})
